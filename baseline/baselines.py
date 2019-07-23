80806a5ac931b896f40a266890c4a377e2b3a8a1
Peter Yoachim <yoachim@uw.edu>
2019-07-23 10:28:17 -0700

    new baseline

---
+++ baseline/baselines.py
@@ -0,0 +1,188 @@
+import numpy as np
+import matplotlib.pylab as plt
+import healpy as hp
+from lsst.sims.featureScheduler.modelObservatory import Model_observatory
+from lsst.sims.featureScheduler.schedulers import Core_scheduler
+from lsst.sims.featureScheduler.utils import standard_goals, calc_norm_factor
+import lsst.sims.featureScheduler.basis_functions as bf
+from lsst.sims.featureScheduler.surveys import (generate_dd_surveys, Greedy_survey,
+                                                Blob_survey)
+from lsst.sims.featureScheduler import sim_runner
+import sys
+import subprocess
+import os
+import argparse
+
+
+def gen_greedy_surveys(nside, nexp=1):
+    """
+    Make a quick set of greedy surveys
+    """
+    target_map = standard_goals(nside=nside)
+    norm_factor = calc_norm_factor(target_map)
+    # Let's remove the bluer filters since this should only be near twilight
+    filters = ['r', 'i', 'z', 'y']
+    surveys = []
+
+    for filtername in filters:
+        bfs = []
+        bfs.append(bf.M5_diff_basis_function(filtername=filtername, nside=nside))
+        bfs.append(bf.Target_map_basis_function(filtername=filtername,
+                                                target_map=target_map[filtername],
+                                                out_of_bounds_val=np.nan, nside=nside,
+                                                norm_factor=norm_factor))
+        bfs.append(bf.Slewtime_basis_function(filtername=filtername, nside=nside))
+        bfs.append(bf.Strict_filter_basis_function(filtername=filtername))
+        # Masks, give these 0 weight
+        bfs.append(bf.Zenith_shadow_mask_basis_function(nside=nside, shadow_minutes=60., max_alt=76.))
+        bfs.append(bf.Moon_avoidance_basis_function(nside=nside, moon_distance=40.))
+
+        bfs.append(bf.Filter_loaded_basis_function(filternames=filtername))
+
+        weights = np.array([3.0, 0.3, 3., 3., 0., 0., 0.])
+        surveys.append(Greedy_survey(bfs, weights, block_size=1, filtername=filtername,
+                                     dither=True, nside=nside, ignore_obs='DD', nexp=nexp))
+
+    return surveys
+
+
+def generate_blobs(nside, mixed_pairs=False, nexp=1, no_pairs=False):
+    target_map = standard_goals(nside=nside)
+    norm_factor = calc_norm_factor(target_map)
+
+    # List to hold all the surveys (for easy plotting later)
+    surveys = []
+
+    # Set up observations to be taken in blocks
+    filter1s = ['u', 'g', 'r', 'i', 'z', 'y']
+    if mixed_pairs:
+        filter2s = [None, 'r', 'i', 'z', None, None]
+    else:
+        filter2s = [None, 'g', 'r', 'i', None, None]
+
+    if no_pairs:
+        filter2s = [None, None, None, None, None, None]
+
+    # Ideal time between taking pairs
+    pair_time = 22.
+    times_needed = [pair_time, pair_time*2]
+    for filtername, filtername2 in zip(filter1s, filter2s):
+        bfs = []
+        bfs.append(bf.M5_diff_basis_function(filtername=filtername, nside=nside))
+        if filtername2 is not None:
+            bfs.append(bf.M5_diff_basis_function(filtername=filtername2, nside=nside))
+        bfs.append(bf.Target_map_basis_function(filtername=filtername,
+                                                target_map=target_map[filtername],
+                                                out_of_bounds_val=np.nan, nside=nside,
+                                                norm_factor=norm_factor))
+        if filtername2 is not None:
+            bfs.append(bf.Target_map_basis_function(filtername=filtername2,
+                                                    target_map=target_map[filtername2],
+                                                    out_of_bounds_val=np.nan, nside=nside,
+                                                    norm_factor=norm_factor))
+        bfs.append(bf.Slewtime_basis_function(filtername=filtername, nside=nside))
+        bfs.append(bf.Strict_filter_basis_function(filtername=filtername))
+        # Masks, give these 0 weight
+        bfs.append(bf.Zenith_shadow_mask_basis_function(nside=nside, shadow_minutes=60., max_alt=76.))
+        bfs.append(bf.Moon_avoidance_basis_function(nside=nside, moon_distance=30.))
+        filternames = [fn for fn in [filtername, filtername2] if fn is not None]
+        bfs.append(bf.Filter_loaded_basis_function(filternames=filternames))
+        if filtername2 is None:
+            time_needed = times_needed[0]
+        else:
+            time_needed = times_needed[1]
+        bfs.append(bf.Time_to_twilight_basis_function(time_needed=time_needed))
+        bfs.append(bf.Not_twilight_basis_function())
+        weights = np.array([3.0, 3.0, .3, .3, 3., 3., 0., 0., 0., 0., 0.])
+        if filtername2 is None:
+            # Need to scale weights up so filter balancing still works properly.
+            weights = np.array([6.0, 0.6, 3., 3., 0., 0., 0., 0., 0.])
+        if filtername2 is None:
+            survey_name = 'blob, %s' % filtername
+        else:
+            survey_name = 'blob, %s%s' % (filtername, filtername2)
+        surveys.append(Blob_survey(bfs, weights, filtername1=filtername, filtername2=filtername2,
+                                   ideal_pair_time=pair_time, nside=nside,
+                                   survey_note=survey_name, ignore_obs='DD', dither=True,
+                                   nexp=nexp))
+
+    return surveys
+
+
+def run_sched(surveys, survey_length=365.25, nside=32, fileroot='baseline_', verbose=False,
+              extra_info=None):
+    years = np.round(survey_length/365.25)
+    scheduler = Core_scheduler(surveys, nside=nside)
+    n_visit_limit = None
+    observatory = Model_observatory(nside=nside)
+    observatory, scheduler, observations = sim_runner(observatory, scheduler,
+                                                      survey_length=survey_length,
+                                                      filename=fileroot+'%iyrs.db' % years,
+                                                      delete_past=True, n_visit_limit=n_visit_limit,
+                                                      verbose=verbose, extra_info=extra_info)
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--nexp", type=int, default=1, help="Number of exposures per visit")
+    parser.add_argument("--Pairs", dest='pairs', action='store_true')
+    parser.add_argument("--noPairs", dest='pairs', action='store_false')
+    parser.set_defaults(pairs=True)
+    parser.add_argument("--mixedPairs", dest='mixedPairs', action='store_true')
+    parser.add_argument("--nomixedPairs", dest='mixedPairs', action='store_false')
+    parser.set_defaults(mixedPairs=True)
+    parser.add_argument("--verbose", dest='verbose', action='store_true')
+    parser.set_defaults(verbose=False)
+    parser.add_argument("--survey_length", type=float, default=365.25*10)
+    parser.add_argument("--outDir", type=str, default="")
+
+    args = parser.parse_args()
+    nexp = args.nexp
+    Pairs = args.pairs
+    mixedPairs = args.mixedPairs
+    survey_length = args.survey_length  # Days
+    outDir = args.outDir
+    verbose = args.verbose
+
+    nside = 32
+
+    extra_info = {}
+    exec_command = ''
+    for arg in sys.argv:
+        exec_command += ' ' + arg
+    extra_info['exec command'] = exec_command
+    extra_info['git hash'] = subprocess.check_output(['git', 'rev-parse', 'HEAD'])
+    extra_info['file executed'] = os.path.realpath(__file__)
+
+    if Pairs:
+        if mixedPairs:
+            # mixed pairs.
+            if nexp > 1:
+                fileroot = 'baseline_%iexp' % nexp
+            else:
+                filerood = 'baseline_'
+            greedy = gen_greedy_surveys(nside, nexp=nexp)
+            ddfs = generate_dd_surveys(nside=nside, nexp=nexp)
+            blobs = generate_blobs(nside, nexp=nexp, mixed_pairs=True)
+            surveys = [ddfs, blobs, greedy]
+            run_sched(surveys, survey_length=survey_length, verbose=verbose,
+                      fileroot=os.path.join(outDir, fileroot), extra_info=extra_info,
+                      nside=nside)
+        else:
+            # Same filter for pairs
+            greedy = gen_greedy_surveys(nside, nexp=nexp)
+            ddfs = generate_dd_surveys(nside=nside, nexp=nexp)
+            blobs = generate_blobs(nside, nexp=nexp)
+            surveys = [ddfs, blobs, greedy]
+            run_sched(surveys, survey_length=survey_length, verbose=verbose,
+                      fileroot=os.path.join(outDir, 'baseline_%iexp_pairsame_' % nexp), extra_info=extra_info,
+                      nside=nside)
+    else:
+        greedy = gen_greedy_surveys(nside, nexp=nexp)
+        ddfs = generate_dd_surveys(nside=nside, nexp=nexp)
+        blobs = generate_blobs(nside, nexp=nexp, no_pairs=True)
+        surveys = [ddfs, blobs, greedy]
+        run_sched(surveys, survey_length=survey_length, verbose=verbose,
+                  fileroot=os.path.join(outDir, 'baseline_%iexp_nopairs_' % nexp), extra_info=extra_info,
+                  nside=nside)
